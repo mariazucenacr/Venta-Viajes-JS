3363892c5b620a74fd69128ce783545034298c34
Mariazucena Calderon <azucenaleo96@gmail.com>
2024-09-03 16:48:07 -0500

    Se realizaron mejoras en el codigo

--- script.js
+++ script.js
@@ -7,29 +7,32 @@ let enlaces = document.querySelectorAll("a");
 let tituloElemento = document.getElementById("titulo");
 let subTituloElemento = document.getElementById("subtitulo");
 let parrafoElemento = document.getElementById("parrafo");
+let precioElemento = document.getElementById("precio");
+console.log(enlaces)
 
 //vamos hacer un bucle para la consulta de cada uno de los elementos
 //agregaremos un elemento CLICK a cada enlace
 enlaces.forEach(function (enlace) {
-    enlace.addEventListener("click", function () {
+    enlace.addEventListener('click', function () {
         //bucle interno para remover el active a los elementos
-        enlace.forEach(function (enlace) {
-            enlace.classList.remove("active");
+        enlaces.forEach(function (enlace) {
+            enlace.classList.remove('active');
         });
+ 
+        //luego agregamos clase 'active' donde se hizo el click 
+        //this vendria a ser "enlace" al que hicimos click
+        this.classList.add('active')
+
+        //traer la informacion del objeto correspondiente a la eleccion segun el "enlace" (con la funcion de abajo "obtenerContenido")
+        let contenido = obtenerContenido(this.textContent)
+
+        //mostrar el contenido en el DOM
+        tituloElemento.innerHTML = contenido.titulo
+        subTituloElemento.innerHTML = contenido.subtitulo
+        parrafoElemento.innerHTML = contenido.parrafo
+        precioElemento.innerHTML = contenido.precio
+
     });
-
-    //luego agregamos clase 'active' donde se hizo el click 
-    //this vendria a ser "enlace" al que hicimos click
-    this.classList.add('active')
-
-    //traer la informacion del objeto correspondiente a la eleccion segun el "enlace" (con la funcion de abajo "obtenerContenido")
-    let contenido = obtenerContenido(this.textContent)
-
-    //mostrar el contenido en el DOM
-    tituloElemento.innerHTML = contenido.tituloElemento
-    subTituloElemento.innerHTML = contenido.subTituloElemento
-    parrafoElemento.innerHTML = contenido.parrafoElemento
-
 });
 
 //funcion para traer la informacion correcta desde ciudades.js
